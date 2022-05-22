61eb2df329fdd2ff4822b44ad5bb948370284475
= <benfouch@Bens-MacBook-Air.local>
2022-05-21 19:43:40 -0500

    started making objs

--- main.py
+++ main.py
@@ -1,38 +1,60 @@
 import tkinter
 from tkinter import *
 import time
+import random
+
+class Bug:
+    def __init__(self, pos, speed, shape, canvas):
+        self.pos = pos
+        self.speed = speed
+        self.shape = shape
+        self.canvas = canvas
+
+    def move(self):
+        # move it to food
+        self.canvas.move(self.shape, self.speed, self.speed)
+
+class Food:
+    def __init__(self, healing_points, ):
+
+
 
 def main():
     win=tkinter.Tk()
 
-    canvas = Canvas(win, width=500, height=500)
+    canvas = Canvas(win, width=700, height=700)
 
-    pos = (100, 100)
     radius = 5
+    bugs = []
+    food = []
 
-    oval=canvas.create_oval(pos[0]-radius, pos[1]-radius, pos[0]+radius, pos[1]+radius, fill='blue')
+    for i in range(5):
+        pos = (random.random()*650, random.random()*650)
+        speed = random.random()*15
+        oval=canvas.create_oval(pos[0]-radius, pos[1]-radius, pos[0]+radius, pos[1]+radius, fill='blue')
+        bugs += [Bug(pos, speed, oval, canvas)]
+
+
+    for i in range(10):
+        pos = (random.random()*650, random.random()*650)
+        food += []
+
+
     canvas.pack()
 
-    # sleep(5)
-
-    # print("test")
-
-    # win.after(10000, test(win, 0, canvas, pos, radius, oval))
-
-    win.after(5000, test(win, 0, canvas, oval))
-
+    test(win, 0, bugs)
     win.mainloop()
 
 
-def test(win, count, canvas, oval):
-    i = count
-    #print("test")
-    canvas.move(oval, 10, 10)
+def test(win, count, bugs):
+    for b in bugs:
+        b.move()
+
     win.update()
 
-    if i < 20:
-        time.sleep(.5)
-        test(win, count+1, canvas, oval)
+    if count < 20:
+        time.sleep(.3)
+        test(win, count+1, bugs)
 
 
 if __name__ == '__main__':
